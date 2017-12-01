a6e83bc4e8d22986388a20f2e46e380c1a86d652
康怀帅 <khs1994@khs1994.com>
2017-12-01 08:57:05 -0600

    7.2.0
    7.2.0 (#7)

--- update.sh
+++ update.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-VERSION=7.1
+VERSION=7.2
 
-rm -rf ${VERSION} && mkdir -p ${VERSION}/alpine3.4
+rm -rf ${VERSION} && mkdir -p ${VERSION}/alpine3.6
 
-cp -a ~/lnmp/dockerfile/php-fpm/. ${VERSION}/alpine3.4
+cp -a ~/lnmp/dockerfile/php-fpm/. ${VERSION}/alpine3.6
 
 # 国外构建必须换为原始源
 
-sed -i "" "s/RUN sed/# RUN sed/g" ${VERSION}/alpine3.4/Dockerfile
+sed -i "" "s/RUN sed/# RUN sed/g" ${VERSION}/alpine3.6/Dockerfile
