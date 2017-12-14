6d37f6d2f5d2ff07204aacf8665f859c1f41fae1
khs1994 <khs1994@khs1994.com>
2017-12-14 15:21:14 +0800

    Based alpine 3.7

--- 7.1/alpine3.4/Changelog.md
+++
@@ -1,2 +0,0 @@
-* `xdebug 扩展` 默认不启用
-* 以 `Alpine` 为基础，弃用 `Debian`。
--- update.sh
+++ update.sh
@@ -2,10 +2,10 @@
 
 VERSION=7.2
 
-rm -rf ${VERSION} && mkdir -p ${VERSION}/alpine3.6
+rm -rf ${VERSION} && mkdir -p ${VERSION}/alpine3.7
 
-cp -a ~/lnmp/dockerfile/php-fpm/. ${VERSION}/alpine3.6
+cp -a ~/lnmp/dockerfile/php-fpm/. ${VERSION}/alpine3.7
 
 # 国外构建必须换为原始源
 
-sed -i "" "s/RUN sed/# RUN sed/g" ${VERSION}/alpine3.6/Dockerfile
+sed -i "" "s/RUN sed/# RUN sed/g" ${VERSION}/alpine3.7/Dockerfile
